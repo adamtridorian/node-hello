bcfa8e14c724e06a8d875ff9a428ce981333f567
Adam Morgan <adammorgan@Adams-MacBook-Air.local>
2024-07-04 11:43:19 +0700

    hello intract

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y nodejs npm
 cd /home/adam_boonchaya/node-hello
 sudo ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 sudo git config --global --add safe.directory /home/adam_boonchaya/node-hello
@@ -8,4 +10,12 @@ sudo cp -r * /var/www/html
 sudo rm -rf /var/www/html/.git
 sudo chown -R www-data:www-data /var/www/html/*
 cd /var/www/html
-sudo npm start
+
+# Start the Node.js application using pm2
+sudo pm2 start /var/www/html/your-app-file.js --name "node-hello"
+
+# Ensure pm2 restarts the app on reboot
+sudo pm2 startup systemd
+sudo pm2 save
+
+
