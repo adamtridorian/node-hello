39c82c225b5b7b44bed6e636648e95b73fe59865
Adam Morgan <adammorgan@Adams-MacBook-Air.local>
2024-07-04 11:15:55 +0700

    npm install

--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@ cd /home/adam_boonchaya/node-hello
 sudo ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 sudo git config --global --add safe.directory /home/adam_boonchaya/node-hello
 sudo git pull
+sudo npm install
 sudo cp -r * /var/www/html
 sudo rm -rf /var/www/html/.git
 sudo chown -R www-data:www-data /var/www/html/*
+cd /var/www/html
+sudo npm start
