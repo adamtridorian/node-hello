975b0b3bbf3950e0f2bf6f58abd316e54e4a9d69
Adam Morgan <adammorgan@Adams-MacBook-Air.local>
2024-07-04 11:09:58 +0700

    cloud build

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/adam_boonchaya/node-hello
+sudo ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+sudo git config --global --add safe.directory /home/adam_boonchaya/node-hello
+sudo git pull
+sudo cp -r * /var/www/html
+sudo rm -rf /var/www/html/.git
+sudo chown -R www-data:www-data /var/www/html/*
